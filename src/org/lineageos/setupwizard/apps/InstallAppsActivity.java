c03961be67917921539eabcfe8861054cd92555c
Torsten Grote <t@grobox.de>
2019-03-11 15:23:19 -0300

    Add step for installing apps from local F-Droid repo
    Co-authored-by: Aayush Gupta <aayushgupta219@gmail.com>
    Co-authored-by: Chirayu Desai <chirayudesai1@gmail.com>
    Co-authored-by: Michael Bestas <mkbestas@gmail.com>
    Co-authored-by: Oliver Scott <olivercscott@gmail.com>
    Co-authored-by: Tommy Webb <tommy@calyxinstitute.org>
    Change-Id: I70cf6523188431fe1745971f103a61950a69a11d

---
+++ src/org/lineageos/setupwizard/apps/InstallAppsActivity.java
@@ -0,0 +1,21 @@
+/*
+ * SPDX-FileCopyrightText: 2019-2024 The Calyx Institute
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.setupwizard.apps;
+
+import static org.lineageos.setupwizard.SetupWizardApp.ACTION_SETUP_INSTALL;
+
+import android.content.Intent;
+
+import org.lineageos.setupwizard.SubBaseActivity;
+
+public class InstallAppsActivity extends SubBaseActivity {
+
+    @Override
+    protected void onStartSubactivity() {
+        Intent intent = new Intent(ACTION_SETUP_INSTALL);
+        startSubactivity(intent);
+    }
+}
