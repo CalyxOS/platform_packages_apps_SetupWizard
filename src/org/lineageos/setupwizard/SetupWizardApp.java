fefa7d5f3537db66258fcb6aedb0dabb7e8bdf59
Oliver Scott <olivercscott@gmail.com>
2020-12-26 16:15:25 -0500

    Turn off OEM unlocking during initial setup if bootloader is locked
    * CalyxOS is meant to be used with a locked bootloader, and OEM unlocking
      set to off
    * Does what Settings -> Developer Options -> OEM unlocking off does but silently,
      wihout having to enable Developer Options.
    * If you've locked the bootloader, and the device has booted into SetupWizard,
      it should be safe to run this since at that point you can most definitely
      get into Settings and run this yourself as well, and we do have the same
      kinds of checks here.
    
    Change-Id: I739d0808fc5c66656128f755451441de96764a90
    (cherry picked from commit 23fb0230f62881d7dc77309942263f23657990db)

--- src/org/lineageos/setupwizard/SetupWizardApp.java
+++ src/org/lineageos/setupwizard/SetupWizardApp.java
@@ -25,6 +25,7 @@ import android.content.ComponentName;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.UserHandle;
+import android.service.oemlock.OemLockManager;
 import android.util.Log;
 
 import org.lineageos.setupwizard.util.NetworkMonitor;
@@ -106,6 +107,12 @@ public class SetupWizardApp extends Application {
         sStatusBarManager = SetupWizardUtils.disableStatusBar(this);
         mHandler.postDelayed(mRadioTimeoutRunnable, SetupWizardApp.RADIO_READY_TIMEOUT);
         scheduleIndexUpdateJob();
+        // If the bootloader is locked, and OEM unlocking is allowed, turn it off
+        if (SetupWizardUtils.isOwner()
+            && !SetupWizardUtils.isBootloaderUnlocked(this)
+            && SetupWizardUtils.isOemunlockAllowed(this)) {
+            getSystemService(OemLockManager.class).setOemUnlockAllowedByUser(false);
+        }
     }
 
     public static StatusBarManager getStatusBarManager() {
