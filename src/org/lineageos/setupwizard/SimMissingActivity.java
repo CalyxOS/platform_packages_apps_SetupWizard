8f98847578282a0474412b039b48e9d42ab29cb1
Oliver Scott <olivercscott@gmail.com>
2020-12-28 22:18:05 -0500

    Add button to setup eSIM on missing SIM page
    Co-authored-by: Chirayu Desai <chirayudesai1@gmail.com>
    Co-authored-by: Michael Bestas <mkbestas@gmail.com>
    Change-Id: I61072d392a4eff5e5223782c2bda8c0d3ca45d1f

--- src/org/lineageos/setupwizard/SimMissingActivity.java
+++ src/org/lineageos/setupwizard/SimMissingActivity.java
@@ -6,18 +6,54 @@
 
 package org.lineageos.setupwizard;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemProperties;
+import android.service.euicc.EuiccService;
+import android.telephony.euicc.EuiccManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.setupcompat.template.FooterButtonStyleUtils;
+import com.google.android.setupcompat.util.WizardManagerHelper;
 
 import org.lineageos.setupwizard.util.SetupWizardUtils;
 
-public class SimMissingActivity extends BaseSetupWizardActivity {
+public class SimMissingActivity extends SubBaseActivity {
+
+    public static final String TAG = SimMissingActivity.class.getSimpleName();
+
+    // From com.android.settings.network.telephony.MobileNetworkUtils
+    // System Property which is used to decide whether the default eSIM UI will be shown,
+    // the default value is false.
+    private static final String KEY_ENABLE_ESIM_UI_BY_DEFAULT =
+            "esim.enable_esim_system_ui_by_default";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getGlifLayout().setDescriptionText(getString(R.string.sim_missing_summary));
         if (!SetupWizardUtils.simMissing(this)) {
             finishAction(RESULT_OK);
+        }
+    }
+
+    @Override
+    protected void onStartSubactivity() {
+        setNextAllowed(true);
+        EuiccManager euiccManager = (EuiccManager) getSystemService(Context.EUICC_SERVICE);
+        if (euiccManager.isEnabled() /*&& NetworkMonitor.getInstance().isNetworkConnected()*/
+                && SystemProperties.getBoolean(KEY_ENABLE_ESIM_UI_BY_DEFAULT, true)) {
+            getGlifLayout().setDescriptionText(getString(R.string.sim_missing_full_description,
+                    getString(R.string.sim_missing_summary),
+                    getString(R.string.euicc_summary)));
+            Button setupEuiccButton = findViewById(R.id.setup_euicc);
+            setupEuiccButton.setOnClickListener(v -> launchEuiccSetup());
+            FooterButtonStyleUtils.applyPrimaryButtonPartnerResource(this, setupEuiccButton, true);
+        } else {
+            getGlifLayout().setDescriptionText(getString(R.string.sim_missing_summary));
+            findViewById(R.id.setup_euicc).setVisibility(View.GONE);
         }
     }
 
@@ -34,6 +70,16 @@ public class SimMissingActivity extends BaseSetupWizardActivity {
     @Override
     protected int getIconResId() {
         return R.drawable.ic_sim;
+    }
+
+    private void launchEuiccSetup() {
+        Intent intent = new Intent(EuiccService.ACTION_PROVISION_EMBEDDED_SUBSCRIPTION);
+        intent.putExtra(EuiccManager.EXTRA_FORCE_PROVISION, true);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startSubactivity(intent);
+        } else {
+            Log.e(TAG, "No activity available to handle " + intent.getAction());
+        }
     }
 
 }
