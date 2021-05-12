e9169a945898d0ae2bda84e78f4a1c65fc1a810d
Oliver Scott <olivercscott@gmail.com>
2021-05-12 13:42:51 -0400

    Schedule F-Droid index update on initialization and network connection
    Change-Id: Idb2300885cb2d249d0d495b9700cd13661a649a1

--- src/org/calyxos/setupwizard/util/NetworkMonitor.java
+++ src/org/calyxos/setupwizard/util/NetworkMonitor.java
@@ -27,6 +27,8 @@ import android.net.NetworkInfo;
 import android.os.Handler;
 import android.util.Log;
 
+import org.calyxos.setupwizard.SetupWizardApp;
+
 public class NetworkMonitor {
 
     public static final String TAG = NetworkMonitor.class.getSimpleName();
@@ -92,6 +94,8 @@ public class NetworkMonitor {
         }
         mNetworkConnected = true;
         mNetworkInfo = ni;
+
+        ((SetupWizardApp) mContext).scheduleIndexUpdateJob();
     }
 
     private void onNetworkDisconnected() {
