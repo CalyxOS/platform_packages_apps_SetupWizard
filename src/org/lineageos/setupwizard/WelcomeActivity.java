20eb3df12373d2218655de64f649e2b4f478e946
Chirayu Desai <chirayudesai1@gmail.com>
2021-07-16 23:16:51 +0530

    WelcomeActivity: Optionally show additional details
    * Long press of brand logo (CalyxOS logo in the middle)
      shows you
      * Bootloader lock status
      * OEM unlocking toggle status
    * It also lets you hit a button to factory reset and power off,
      which does exactly what the name implies
    
    Change-Id: I6ec9ec98dffe1be3e7c516b31fa4612edd079add
    (cherry picked from commit 5fc74855659ac750a4da9735c65c1663b22f9a8e)

--- src/org/lineageos/setupwizard/WelcomeActivity.java
+++ src/org/lineageos/setupwizard/WelcomeActivity.java
@@ -17,12 +17,17 @@
 
 package org.lineageos.setupwizard;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.Rect;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.setupcompat.util.SystemBarHelper;
@@ -36,6 +41,8 @@ public class WelcomeActivity extends BaseSetupWizardActivity {
     public static final String TAG = WelcomeActivity.class.getSimpleName();
 
     private View mRootView;
+    private ImageView mBrandLogoView;
+    private FrameLayout mPage;
     private GestureDetector mGestureDetector;
     private MotionEvent previousTapEvent;
     private int consecutiveTaps;
@@ -45,6 +52,8 @@ public class WelcomeActivity extends BaseSetupWizardActivity {
         super.onCreate(savedInstanceState);
         SystemBarHelper.setBackButtonVisible(getWindow(), false);
         mRootView = findViewById(R.id.root_view);
+        mBrandLogoView = findViewById(R.id.brand_logo);
+        mPage = findViewById(R.id.page);
         setNextText(R.string.start);
         setSkipText(R.string.emergency_call);
         findViewById(R.id.start).setOnClickListener(view -> onNextPressed());
@@ -72,7 +81,7 @@ public class WelcomeActivity extends BaseSetupWizardActivity {
                     } else {
                         consecutiveTaps = 1;
                     }
-                    if (consecutiveTaps == 4) {
+                    if (Build.IS_DEBUGGABLE && consecutiveTaps == 4) {
                         Toast.makeText(WelcomeActivity.this, R.string.skip_setupwizard,
                                 Toast.LENGTH_LONG).show();
                         SetupWizardUtils.finishSetupWizard(WelcomeActivity.this);
@@ -88,11 +97,25 @@ public class WelcomeActivity extends BaseSetupWizardActivity {
                 previousTapEvent = MotionEvent.obtain(e);
                 return false;
             }
+
+            @Override
+            public void onLongPress(MotionEvent e) {
+                Rect viewRect = new Rect();
+                int[] leftTop = new int[2];
+
+                findViewById(R.id.factory_reset).setOnClickListener(v -> factoryResetAndShutdown());
+
+                mBrandLogoView.getLocationOnScreen(leftTop);
+                viewRect.set(
+                        leftTop[0], leftTop[1], leftTop[0] + mBrandLogoView.getWidth(), leftTop[1] + mBrandLogoView.getHeight());
+                if (viewRect.contains((int) e.getX(), (int) e.getY())) {
+                    setupDetails();
+                    mPage.setVisibility(View.VISIBLE);
+                }
+            }
         });
-        if (Build.IS_DEBUGGABLE) {
-            mRootView.setOnTouchListener((v, event) ->
-                    mGestureDetector.onTouchEvent(event));
-        }
+
+        mRootView.setOnTouchListener((v, event) -> mGestureDetector.onTouchEvent(event));
     }
 
     @Override
@@ -115,5 +138,48 @@ public class WelcomeActivity extends BaseSetupWizardActivity {
         return (deltaX * deltaX + deltaY * deltaY >=
                 (mRootView.getWidth() * mRootView.getWidth()) / 2
                 && deltaTime < TimeUnit.SECONDS.toMillis(1));
+    }
+
+    private void setupDetails() {
+        // CalyxOS is meant to be used with a locked bootloader and OEM Unlocking disabled
+        final Boolean bootloaderUnlocked = SetupWizardUtils.isBootloaderUnlocked(this);
+        final Boolean oemunlockAllowed = SetupWizardUtils.isOemunlockAllowed(this);
+        final TextView bootloaderStatus = (TextView) findViewById(R.id.bootloader_status);
+        final TextView oemunlockStatus = (TextView) findViewById(R.id.oemunlock_status);
+
+        if (bootloaderUnlocked) {
+            // Bootloader unlocked, bad.
+            bootloaderStatus.setText(R.string.bootloader_unlocked);
+            bootloaderStatus.setTextColor(Color.RED);
+            // OEM Unlocking is greyed out when bootloader is unlocked
+            oemunlockStatus.setText(R.string.oemunlock_na);
+            oemunlockStatus.setTextColor(Color.YELLOW);
+        } else {
+            // Bootloader locked, good.
+            bootloaderStatus.setText(R.string.bootloader_locked);
+            bootloaderStatus.setTextColor(Color.GREEN);
+            if (oemunlockAllowed) {
+                // OEM Unlocking allowed, bad.
+                oemunlockStatus.setText(R.string.oemunlock_allowed);
+                oemunlockStatus.setTextColor(Color.RED);
+            } else {
+                // OEM Unlocking not allowed, good.
+                oemunlockStatus.setText(R.string.oemunlock_notallowed);
+                oemunlockStatus.setTextColor(Color.GREEN);
+            }
+        }
+    }
+
+    private void factoryResetAndShutdown() {
+        // com.android.settings.MasterClearConfirm.doMasterClear()
+        Intent intent = new Intent(Intent.ACTION_FACTORY_RESET);
+        intent.setPackage("android");
+        intent.addFlags(Intent.FLAG_RECEIVER_FOREGROUND);
+        intent.putExtra(Intent.EXTRA_REASON, "SetupWizard");
+        intent.putExtra(Intent.EXTRA_WIPE_ESIMS, false);
+        // com.android.server.MasterClearReceiver
+        intent.putExtra("shutdown", true);
+        sendBroadcast(intent);
+        // Intent handling is asynchronous -- assume it will happen soon.
     }
 }
