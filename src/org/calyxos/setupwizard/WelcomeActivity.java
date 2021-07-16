ade0b83260553ce19fe51fc46cc09ba56fa1a899
Chirayu Desai <chirayudesai1@gmail.com>
2021-07-16 23:16:51 +0530

    WelcomeActivity: Optionally show additional details
    * Long press of brand logo (CalyxOS logo in the middle)
      shows you
      * Bootloader lock status
      * OEM unlocking toggle status
    * It also lets you hit a button to factory reset and power off,
      which does exactly what the name implies
    
    Change-Id: I6ec9ec98dffe1be3e7c516b31fa4612edd079add

--- src/org/calyxos/setupwizard/WelcomeActivity.java
+++ src/org/calyxos/setupwizard/WelcomeActivity.java
@@ -17,11 +17,16 @@
 
 package org.calyxos.setupwizard;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.calyxos.setupwizard.util.EnableAccessibilityController;
@@ -34,6 +39,8 @@ public class WelcomeActivity extends BaseSetupWizardActivity {
     public static final String TAG = WelcomeActivity.class.getSimpleName();
 
     private View mRootView;
+    private ImageView mBrandLogoView;
+    private FrameLayout mPage;
     private EnableAccessibilityController mEnableAccessibilityController;
     private GestureDetector mGestureDetector;
     private MotionEvent previousTapEvent;
@@ -43,6 +50,8 @@ public class WelcomeActivity extends BaseSetupWizardActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mRootView = findViewById(R.id.root);
+        mBrandLogoView = findViewById(R.id.brand_logo);
+        mPage = findViewById(R.id.page);
         setNextText(R.string.next);
         setBackText(R.string.emergency_call);
         setBackDrawable(null);
@@ -82,7 +91,24 @@ public class WelcomeActivity extends BaseSetupWizardActivity {
                 previousTapEvent = MotionEvent.obtain(e);
                 return false;
             }
+
+            @Override
+            public void onLongPress(MotionEvent e) {
+                Rect viewRect = new Rect();
+                int[] leftTop = new int[2];
+
+                findViewById(R.id.factory_reset).setOnClickListener(v -> factoryResetAndShutdown());
+
+                mBrandLogoView.getLocationOnScreen(leftTop);
+                viewRect.set(
+                        leftTop[0], leftTop[1], leftTop[0] + mBrandLogoView.getWidth(), leftTop[1] + mBrandLogoView.getHeight());
+                if (viewRect.contains((int) e.getX(), (int) e.getY())) {
+                    setupDetails();
+                    mPage.setVisibility(View.VISIBLE);
+                }
+            }
         });
+
         mRootView.setOnTouchListener((v, event) ->
                 mEnableAccessibilityController.onTouchEvent(event));
         mRootView.setOnTouchListener((v, event) ->
@@ -113,5 +139,41 @@ public class WelcomeActivity extends BaseSetupWizardActivity {
         long deltaTime = currentTapEvent.getEventTime() - previousTapEvent.getEventTime();
         return (deltaX * deltaX + deltaY * deltaY >= (mRootView.getWidth() * mRootView.getWidth()) / 2
                 && deltaTime < TimeUnit.SECONDS.toMillis(1));
+    }
+
+    private void setupDetails() {
+        // CalyxOS is meant to be used with a locked bootloader and OEM Unlocking disabled
+        final Boolean bootloaderUnlocked = SetupWizardUtils.isBootloaderUnlocked(this);
+        final TextView bootloaderStatus = (TextView) findViewById(R.id.bootloader_status);
+        if (bootloaderUnlocked) {
+            bootloaderStatus.setText(R.string.bootloader_unlocked);
+            bootloaderStatus.setTextColor(Color.RED);
+        } else {
+            bootloaderStatus.setText(R.string.bootloader_locked);
+            bootloaderStatus.setTextColor(Color.GREEN);
+        }
+
+        final Boolean oemunlockAllowed = SetupWizardUtils.isOemunlockAllowed(this);
+        final TextView oemunlockStatus = (TextView) findViewById(R.id.oemunlock_status);
+        if (oemunlockAllowed) {
+            oemunlockStatus.setText(R.string.oemunlock_allowed);
+            oemunlockStatus.setTextColor(Color.GREEN);
+        } else {
+            oemunlockStatus.setText(R.string.oemunlock_notallowed);
+            oemunlockStatus.setTextColor(Color.RED);
+        }
+    }
+
+    private void factoryResetAndShutdown() {
+        // com.android.settings.MasterClearConfirm.doMasterClear()
+        Intent intent = new Intent(Intent.ACTION_FACTORY_RESET);
+        intent.setPackage("android");
+        intent.addFlags(Intent.FLAG_RECEIVER_FOREGROUND);
+        intent.putExtra(Intent.EXTRA_REASON, "SetupWizard");
+        intent.putExtra(Intent.EXTRA_WIPE_ESIMS, false);
+        // com.android.server.MasterClearReceiver
+        intent.putExtra("shutdown", true);
+        sendBroadcast(intent);
+        // Intent handling is asynchronous -- assume it will happen soon.
     }
 }
