LineageOS/android_packages_apps_SetupWizard

Changes for new network setup flow
* Remove mobile data/choose data sim pages
* Rename Wi-Fi setup to network setup
* Switch to the new NetworkProviderSettings which contains
  both Wi-Fi and mobile data setup.

Change-Id: I1c618d629628740e39dbed313d468d1efe971ed7

src/org/lineageos/setupwizard/SetupWizardApp.java
@@ -38,7 +38,7 @@ public class SetupWizardApp extends Application {
     public static final String ACTION_SETUP_COMPLETE =
             "org.lineageos.setupwizard.LINEAGE_SETUP_COMPLETE";
     public static final String ACTION_FINISHED = "org.lineageos.setupwizard.SETUP_FINISHED";
-    public static final String ACTION_SETUP_WIFI = "android.net.wifi.SETUP_WIFI_NETWORK";
+    public static final String ACTION_SETUP_NETWORK = "android.settings.NETWORK_PROVIDER_SETUP";
     public static final String ACTION_SETUP_BIOMETRIC = "android.settings.BIOMETRIC_ENROLL";
     public static final String ACTION_SETUP_LOCKSCREEN = "com.android.settings.SETUP_LOCK_SCREEN";
     public static final String ACTION_RESTORE_FROM_BACKUP =
@@ -64,7 +64,7 @@ public class SetupWizardApp extends Application {
     public static final String ENABLE_RECOVERY_UPDATE = "enable_recovery_update";
     public static final String UPDATE_RECOVERY_PROP = "persist.vendor.recovery_update";
 
-    public static final int REQUEST_CODE_SETUP_WIFI = 0;
+    public static final int REQUEST_CODE_SETUP_NETWORK = 0;
     public static final int REQUEST_CODE_SETUP_CAPTIVE_PORTAL = 4;
     public static final int REQUEST_CODE_SETUP_BLUETOOTH = 5;
     public static final int REQUEST_CODE_SETUP_BIOMETRIC = 7;

CalyxOS/platform_packages_apps_SetupWizard

src/org/lineageos/setupwizard/SetupWizardApp.java
@@ -38,7 +38,7 @@ public class SetupWizardApp extends Application {
     public static final String ACTION_SETUP_COMPLETE =
             "org.lineageos.setupwizard.LINEAGE_SETUP_COMPLETE";
     public static final String ACTION_FINISHED = "org.lineageos.setupwizard.SETUP_FINISHED";
-    public static final String ACTION_SETUP_WIFI = "android.net.wifi.SETUP_WIFI_NETWORK";
+    public static final String ACTION_SETUP_NETWORK = "android.settings.NETWORK_PROVIDER_SETTINGS";
     public static final String ACTION_SETUP_BIOMETRIC = "android.settings.BIOMETRIC_ENROLL";
     public static final String ACTION_SETUP_LOCKSCREEN = "com.android.settings.SETUP_LOCK_SCREEN";
     public static final String ACTION_RESTORE_FROM_BACKUP =
@@ -65,7 +65,7 @@ public class SetupWizardApp extends Application {
     public static final String ENABLE_RECOVERY_UPDATE = "enable_recovery_update";
     public static final String UPDATE_RECOVERY_PROP = "persist.vendor.recovery_update";
 
-    public static final int REQUEST_CODE_SETUP_WIFI = 0;
+    public static final int REQUEST_CODE_SETUP_NETWORK = 0;
     public static final int REQUEST_CODE_SETUP_CAPTIVE_PORTAL = 4;
     public static final int REQUEST_CODE_SETUP_BLUETOOTH = 5;
     public static final int REQUEST_CODE_SETUP_BIOMETRIC = 7;