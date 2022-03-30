639d59c9a9872b84cdeaf8aa931eb64a939ddb99
Tom Pratt <tom.pratt@outlook.com>
2022-03-30 14:20:28 +0200

    Don't disable mobile data, AOSP has props for this
    Mobile data during and after setupwizard can be controlled with:
    
    ro.com.android.mobiledata
    ro.com.android.prov_mobiledata
    
    Change-Id: Ia4ef25f55ab32e8c13f9683b5db73438a29a192b

--- src/org/lineageos/setupwizard/SetupWizardApp.java
+++ src/org/lineageos/setupwizard/SetupWizardApp.java
@@ -79,9 +79,6 @@ public class SetupWizardApp extends Application {
             Log.v(TAG, "onCreate()");
         }
         SetupWizardUtils.disableComponentsForMissingFeatures(this);
-        if (SetupWizardUtils.isOwner()) {
-            SetupWizardUtils.setMobileDataEnabled(this, false);
-        }
         sStatusBarManager = SetupWizardUtils.disableStatusBar(this);
         mHandler.postDelayed(mRadioTimeoutRunnable, SetupWizardApp.RADIO_READY_TIMEOUT);
         // If the bootloader is locked, and OEM unlocking is allowed, turn it off
