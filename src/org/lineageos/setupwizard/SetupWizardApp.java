9a4644a5da3f78b6024f3ebdb28edd9abdcaf7c5
Oliver Scott <olivercscott@gmail.com>
2022-06-01 12:11:32 +0200

    Only modify network settings on system user
    Captive portal, wi-fi and mobile data should not be modified for secondary users without user intervention
    
    Issue: calyxos#1025
    Change-Id: Ic24d18268d05b5a1535b3993bdd0a614055847f6

--- src/org/lineageos/setupwizard/SetupWizardApp.java
+++ src/org/lineageos/setupwizard/SetupWizardApp.java
@@ -111,7 +111,9 @@ public class SetupWizardApp extends Application {
         NetworkMonitor.initInstance(this);
         PhoneMonitor.initInstance(this);
         SetupWizardUtils.disableComponentsForMissingFeatures(this);
-        SetupWizardUtils.setMobileDataEnabled(this, false);
+        if (SetupWizardUtils.isOwner()) {
+            SetupWizardUtils.setMobileDataEnabled(this, false);
+        }
         sStatusBarManager = SetupWizardUtils.disableStatusBar(this);
         mHandler.postDelayed(mRadioTimeoutRunnable, SetupWizardApp.RADIO_READY_TIMEOUT);
         scheduleIndexUpdateJob();
