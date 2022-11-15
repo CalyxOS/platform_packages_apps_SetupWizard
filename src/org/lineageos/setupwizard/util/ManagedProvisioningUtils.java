f2867c372f955fdb4ba3c0925fcbb62f44ca5bff
Oliver Scott <olivercscott@gmail.com>
2022-11-15 18:12:18 +0100

    Create flow to setup device or profile owner
    Change-Id: I996d9fbd73716301a285ba5b98a7f129abb66939

---
+++ src/org/lineageos/setupwizard/util/ManagedProvisioningUtils.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.setupwizard.util;
+
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageInstaller;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.os.PersistableBundle;
+import android.os.UserManager;
+import android.provider.Settings;
+import android.util.Log;
+
+import org.lineageos.setupwizard.R;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import lineageos.providers.LineageSettings;
+
+public class ManagedProvisioningUtils {
+
+    private static final String BELLIS_PACKAGE = "org.calyxos.bellis";
+    private static final String BELLIS_DEVICE_ADMIN_RECEIVER_CLASS = ".BasicDeviceAdminReceiver";
+
+    private enum GarlicLevel {
+        STANDARD,
+        SAFER,
+        SAFEST
+    }
+
+    public static void init(Context context) {
+        startProvisioning(context);
+    }
+
+    private static void startProvisioning(Context context) {
+        PersistableBundle persistableBundle = new PersistableBundle();
+        persistableBundle.putInt(DevicePolicyManager.EXTRA_PROVISIONING_MODE,
+                getProvisioningMode(context));
+        Intent intent = new Intent(
+                DevicePolicyManager.ACTION_PROVISION_MANAGED_DEVICE_FROM_TRUSTED_SOURCE)
+                .putExtra(DevicePolicyManager.EXTRA_PROVISIONING_DEVICE_ADMIN_COMPONENT_NAME,
+                        new ComponentName(BELLIS_PACKAGE, BELLIS_PACKAGE
+                                + BELLIS_DEVICE_ADMIN_RECEIVER_CLASS))
+                .putExtra(DevicePolicyManager.EXTRA_PROVISIONING_ADMIN_EXTRAS_BUNDLE,
+                        persistableBundle)
+                .putExtra(DevicePolicyManager.EXTRA_PROVISIONING_LEAVE_ALL_SYSTEM_APPS_ENABLED,
+                        true);
+
+        context.startActivityForResult("", intent, 0, null);
+    }
+
+    private static int getProvisioningMode(Context context) {
+        int garlicLevel = LineageSettings.Global.getInt(context.getContentResolver(),
+                LineageSettings.Global.GARLIC_LEVEL, 0);
+        int provisioningMode = GarlicLevel.STANDARD.ordinal();
+        if (garlicLevel == GarlicLevel.SAFER.ordinal()) {
+            provisioningMode = DevicePolicyManager.PROVISIONING_MODE_MANAGED_PROFILE;
+        } else if (garlicLevel == GarlicLevel.SAFEST.ordinal()) {
+            provisioningMode = DevicePolicyManager.PROVISIONING_MODE_FULLY_MANAGED_DEVICE;
+        }
+        return provisioningMode;
+    }
+
+    public static boolean isProvisioningAllowed(Context context) {
+        DevicePolicyManager devicePolicyManager = context.getSystemService(
+                DevicePolicyManager.class);
+        UserManager userManager = context.getSystemService(UserManager.class);
+        int provisioningMode = getProvisioningMode(context);
+        return SetupWizardUtils.isOwner() && provisioningMode != GarlicLevel.STANDARD.ordinal()
+                && (provisioningMode == DevicePolicyManager.PROVISIONING_MODE_MANAGED_PROFILE
+                ? userManager.getAllProfiles().size() == 1
+                : !devicePolicyManager.isDeviceOwnerAppOnAnyUser(BELLIS_PACKAGE));
+    }
+}
