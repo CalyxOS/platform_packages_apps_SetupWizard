775b870250bde5b5ea88b6c167b67917433f8774
Oliver Scott <olivercscott@gmail.com>
2022-11-15 18:12:18 +0100

    Create flow to setup device or profile owner
    Change-Id: I996d9fbd73716301a285ba5b98a7f129abb66939

--- src/org/lineageos/setupwizard/BaseSetupWizardActivity.java
+++ src/org/lineageos/setupwizard/BaseSetupWizardActivity.java
@@ -124,9 +124,6 @@ public abstract class BaseSetupWizardActivity extends Activity implements Naviga
             logActivityState("onStart");
         }
         super.onStart();
-        if (!SetupWizardUtils.isManagedProfile(this)) {
-            exitIfSetupComplete();
-        }
     }
 
     @Override
@@ -314,15 +311,6 @@ public abstract class BaseSetupWizardActivity extends Activity implements Naviga
     protected void onSetupStart() {
         if (SetupWizardUtils.isOwner()) {
             SetupWizardUtils.disableCaptivePortalDetection(getApplicationContext());
-        }
-    }
-
-    protected void exitIfSetupComplete() {
-        if (WizardManagerHelper.isUserSetupComplete(this)) {
-            Log.i(TAG, "Starting activity with USER_SETUP_COMPLETE=true");
-            startSetupWizardExitActivity();
-            setResult(RESULT_CANCELED, null);
-            finishAllAppTasks();
         }
     }
 
