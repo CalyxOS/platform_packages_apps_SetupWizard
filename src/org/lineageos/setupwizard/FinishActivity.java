0bd8376f17e2046e5c8cc3bbc59a1c1a80c43410
Oliver Scott <olivercscott@gmail.com>
2022-11-15 18:12:18 +0100

    Create flow to setup device or profile owner
    Change-Id: I996d9fbd73716301a285ba5b98a7f129abb66939

--- src/org/lineageos/setupwizard/FinishActivity.java
+++ src/org/lineageos/setupwizard/FinishActivity.java
@@ -31,22 +31,25 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.app.Activity;
 import android.app.WallpaperManager;
+import android.app.admin.DevicePolicyManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.om.IOverlayManager;
 import android.content.pm.ActivityInfo;
+import android.database.ContentObserver;
 import android.graphics.Bitmap;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.ServiceManager;
+import android.provider.Settings;
 import android.view.View;
 import android.view.ViewAnimationUtils;
 import android.widget.ImageView;
 
 import com.google.android.setupcompat.util.WizardManagerHelper;
 
-import org.lineageos.setupwizard.util.SetupWizardUtils;
+import org.lineageos.setupwizard.util.ManagedProvisioningUtils;
 
 public class FinishActivity extends BaseSetupWizardActivity {
 
@@ -93,8 +96,26 @@ public class FinishActivity extends BaseSetupWizardActivity {
 
     @Override
     public void onNavigateNext() {
-        applyForwardTransition(TRANSITION_ID_NONE);
-        startFinishSequence();
+        if (ManagedProvisioningUtils.isProvisioningAllowed(this)) {
+            // When initial setup wizard is complete, finalize garlic-level provisioning.
+            getContentResolver().registerContentObserver(
+                    Settings.Secure.getUriFor(Settings.Secure.USER_SETUP_COMPLETE), false,
+                    new ContentObserver(mHandler) {
+                        @Override
+                        public void onChange(boolean selfChange) {
+                            super.onChange(selfChange);
+                            ManagedProvisioningUtils.finalizeProvisioning(FinishActivity.this);
+                            FinishActivity.this.getContentResolver()
+                                    .unregisterContentObserver(this);
+                            startFinishSequence();
+                        }
+                    });
+            // Initialize garlic-level provisioning.
+            ManagedProvisioningUtils.init(this);
+        } else {
+            applyForwardTransition(TRANSITION_ID_NONE);
+            startFinishSequence();
+        }
     }
 
     private void finishSetup() {
