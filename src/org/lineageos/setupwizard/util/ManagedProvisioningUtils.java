ee2f7c904655e0cc8a7dbc1e167c2b26d7948903
Oliver Scott <olivercscott@gmail.com>
2022-12-08 17:11:47 +0000

    Apply safe garlic level settings on provisioning managed profile
    Change-Id: I6d29786e787aaf2d33da77cb56a5a5027a168f2d
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- src/org/lineageos/setupwizard/util/ManagedProvisioningUtils.java
+++ src/org/lineageos/setupwizard/util/ManagedProvisioningUtils.java
@@ -59,6 +59,10 @@ public class ManagedProvisioningUtils {
 
     public static void init(Context context) {
         installOrbot(context);
+        if (getProvisioningMode(context)
+                == DevicePolicyManager.PROVISIONING_MODE_MANAGED_PROFILE) {
+            setupSafeMode(context);
+        }
         startProvisioning(context);
     }
 
@@ -95,6 +99,15 @@ public class ManagedProvisioningUtils {
         } catch (IOException e) {
             Log.e(TAG, "Failed to install Orbot", e);
         }
+    }
+
+    private static void setupSafeMode(Context context) {
+        Settings.Global.putInt(context.getContentResolver(),
+                Settings.Global.BLUETOOTH_OFF_TIMEOUT, 15000);
+        Settings.Global.putInt(context.getContentResolver(),
+                Settings.Global.WIFI_OFF_TIMEOUT, 15000);
+        LineageSettings.Global.putInt(context.getContentResolver(),
+                LineageSettings.Global.DEVICE_REBOOT_TIMEOUT, 3600000);
     }
 
     private static void startProvisioning(Context context) {
