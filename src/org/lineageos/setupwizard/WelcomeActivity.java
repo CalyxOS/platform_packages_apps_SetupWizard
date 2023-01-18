61fe8a04de2d37630f6f25ab5e426246211ede81
Chirayu Desai <chirayudesai1@gmail.com>
2023-01-18 23:13:00 +0530

    welcome: Use a separate string for work profile start
    * Intentionally not touching this for secondary users
      so that it doesn't look different from the main user.
    * We don't explicitly support that, but let's also not
      go out of our way to break something like this.
    
    Change-Id: I374b45feb7d6dd17e5d03d0f7fbaf6d9e22f1d55

--- src/org/lineageos/setupwizard/WelcomeActivity.java
+++ src/org/lineageos/setupwizard/WelcomeActivity.java
@@ -66,8 +66,12 @@ public class WelcomeActivity extends BaseSetupWizardActivity {
         FooterButtonStyleUtils.applySecondaryButtonPartnerResource(this, emergButton, true);
 
         TextView welcomeTitle = findViewById(R.id.welcome_title);
-        welcomeTitle.setText(getString(R.string.setup_welcome_message,
-                getString(R.string.os_name)));
+        if (SetupWizardUtils.isManagedProfile(this)) {
+            welcomeTitle.setText(getString(R.string.setup_managed_profile_welcome_message));
+        } else {
+            welcomeTitle.setText(getString(R.string.setup_welcome_message,
+                    getString(R.string.os_name)));
+        }
 
         if (Build.IS_DEBUGGABLE) {
             mConsecutiveTapsGestureDetector = new ConsecutiveTapsGestureDetector(
