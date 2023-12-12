a6eef89379c88f05ada41dd580b5555f92bf8f35
Tommy Webb <tommy@calyxinstitute.org>
2023-12-12 17:32:44 -0500

    BiometricActivity: Minimum password complexity
    Do not allow proceeding beyond the biometric enrollment if the
    password complexity is insufficient for the current garlic level.
    
    Requires: I49e81edf24f19e63d7bb682bbbf155cdcde36aca
    Change-Id: Ia8570cea8281a26b1b6352b86bf860fa1afe40c5

--- src/org/lineageos/setupwizard/BiometricActivity.java
+++ src/org/lineageos/setupwizard/BiometricActivity.java
@@ -9,11 +9,35 @@ package org.lineageos.setupwizard;
 import android.content.Intent;
 import android.provider.Settings;
 
+import androidx.activity.result.ActivityResult;
+
+import org.lineageos.setupwizard.util.ManagedProvisioningUtils;
+
 public class BiometricActivity extends SubBaseActivity {
+
+    public static final String TAG = BiometricActivity.class.getSimpleName();
 
     @Override
     protected void onStartSubactivity() {
-        Intent intent = new Intent(Settings.ACTION_BIOMETRIC_ENROLL);
+        Intent intent = ManagedProvisioningUtils.putMinPasswordComplexityToIntent(this,
+                new Intent(Settings.ACTION_BIOMETRIC_ENROLL));
         startSubactivity(intent);
+    }
+
+    @Override
+    protected void onActivityResult(ActivityResult activityResult) {
+        if (activityResult.getResultCode() != RESULT_OK) {
+            final boolean insufficientPassword =
+                    ManagedProvisioningUtils.maybeShowInsufficientPasswordDialog(this,
+                            dialogInterface -> {
+                                ActivityResult newActivityResult = new ActivityResult(
+                                        RESULT_CANCELED, activityResult.getData());
+                                super.onActivityResult(newActivityResult);
+                            });
+            if (insufficientPassword) {
+                return;
+            }
+        }
+        super.onActivityResult(activityResult);
     }
 }
