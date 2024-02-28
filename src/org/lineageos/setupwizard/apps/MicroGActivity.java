ad8554df692e01b7f8b2539609d4a9a0c7eb1ec6
Michael Bestas <mikeioannina@gmail.com>
2024-02-28 16:48:32 +0200

    Update our pages for upstream changes
    Change-Id: Ic513f2c871aafcead01273e8821cb4d246cf97ed

--- src/org/lineageos/setupwizard/apps/MicroGActivity.java
+++ src/org/lineageos/setupwizard/apps/MicroGActivity.java
@@ -1,17 +1,6 @@
 /*
- * Copyright (C) 2019 The Calyx Institute
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * SPDX-FileCopyrightText: 2019 The Calyx Institute
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 package org.lineageos.setupwizard.apps;
@@ -32,7 +21,6 @@ import org.lineageos.setupwizard.R;
 
 public class MicroGActivity extends BaseSetupWizardActivity {
 
-    public static final String TAG = MicroGActivity.class.getSimpleName();
     private static final String[] MICROG_PACKAGES = new String[]{
             "com.google.android.gms",
             "com.google.android.gsf",
