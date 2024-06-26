eec332c318de39c12f387881f03dea5cdd4c1f09
Oliver Scott <olivercscott@gmail.com>
2024-06-26 15:51:38 -0400

    BiometricActivity: Minimum password complexity
    Do not allow proceeding beyond the biometric enrollment if the
    password complexity is insufficient for the current garlic level.
    
    Requires: I49e81edf24f19e63d7bb682bbbf155cdcde36aca
    Change-Id: Ia8570cea8281a26b1b6352b86bf860fa1afe40c5

--- src/org/lineageos/setupwizard/BiometricActivity.java
+++ src/org/lineageos/setupwizard/BiometricActivity.java
@@ -9,11 +9,14 @@ package org.lineageos.setupwizard;
 import android.content.Intent;
 import android.provider.Settings;
 
+import org.lineageos.setupwizard.util.ManagedProvisioningUtils;
+
 public class BiometricActivity extends SubBaseActivity {
 
     @Override
     protected void onStartSubactivity() {
-        Intent intent = new Intent(Settings.ACTION_BIOMETRIC_ENROLL);
+        Intent intent = ManagedProvisioningUtils.putMinPasswordComplexityToIntent(this,
+                new Intent(Settings.ACTION_BIOMETRIC_ENROLL));
         startSubactivity(intent);
     }
 }
