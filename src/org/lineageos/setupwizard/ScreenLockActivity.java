02a2d9e59f7b321671366561f5749dd6cb158327
Tommy Webb <tommy@calyxinstitute.org>
2024-07-08 20:08:17 +0000

    SetupWizard: Check biometric availability when relevant
    Check the availability of biometrics when we need to know, rather than
    checking during initialization and permanently disabling particular
    pages of the wizard. Otherwise, a race condition may cause the system
    to report that the hardware is unavailable, even though it will be
    ready later, when needed.
    
    Issue: calyxos#2544
    Change-Id: I654ce25569b983035f1df453623c7a4acd54931d

--- src/org/lineageos/setupwizard/ScreenLockActivity.java
+++ src/org/lineageos/setupwizard/ScreenLockActivity.java
@@ -8,12 +8,18 @@ package org.lineageos.setupwizard;
 
 import android.content.Intent;
 
+import org.lineageos.setupwizard.util.SetupWizardUtils;
+
 public class ScreenLockActivity extends SubBaseActivity {
 
     private static final String ACTION_SETUP_LOCKSCREEN = "com.android.settings.SETUP_LOCK_SCREEN";
 
     @Override
     protected void onStartSubactivity() {
+        if (SetupWizardUtils.hasBiometric(this)) {
+            nextAction(RESULT_OK);
+            return;
+        }
         Intent intent = new Intent(ACTION_SETUP_LOCKSCREEN);
         startSubactivity(intent);
     }
