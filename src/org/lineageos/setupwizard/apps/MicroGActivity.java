3bdae297646224fccf00445f23f3f48ad54568c9
Tommy Webb <tommy@calyxinstitute.org>
2024-12-19 11:08:39 -0500

    Add online source selection to microG page
    Issue: calyxos#2527
    Change-Id: I0b5b5e3b6ba7640315a6344f18e5698d8f56fcd8

--- src/org/lineageos/setupwizard/apps/MicroGActivity.java
+++ src/org/lineageos/setupwizard/apps/MicroGActivity.java
@@ -13,15 +13,21 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
+
+import androidx.core.content.ContextCompat;
 
 import org.lineageos.setupwizard.BaseSetupWizardActivity;
 import org.lineageos.setupwizard.R;
 import org.lineageos.setupwizard.util.SetupWizardUtils;
 
 public class MicroGActivity extends BaseSetupWizardActivity {
+
+    private static final String ONLINE_SOURCE_UNSET = "<UNSET>";
 
     private static final String[] MICROG_PACKAGES = new String[]{
             "com.google.android.gms",
@@ -33,6 +39,9 @@ public class MicroGActivity extends BaseSetupWizardActivity {
     private Switch enableSwitch;
     private Switch enablePush;
     private Switch enableLocation;
+    private ViewGroup onlineSourcesList;
+    private View onlineSourceRequiredError;
+    private String selectedOnlineSource = ONLINE_SOURCE_UNSET;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -44,12 +53,15 @@ public class MicroGActivity extends BaseSetupWizardActivity {
         enableSwitch = findViewById(R.id.enableSwitch);
         enablePush = findViewById(R.id.enablePush);
         enableLocation = findViewById(R.id.enableLocation);
+        onlineSourcesList = findViewById(R.id.onlineSourcesList);
+        onlineSourceRequiredError = findViewById(R.id.onlineSourceRequiredError);
         enableSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
             enablePush.setEnabled(isChecked);
             enablePush.setChecked(isChecked);
             enableLocation.setEnabled(isChecked);
             enableLocation.setChecked(isChecked);
         });
+        prepareOnlineSourcesList();
 
         final TextView enableSummary = findViewById(R.id.enableSummary);
         final String profileType = SetupWizardUtils.isManagedProfile(this)
@@ -77,6 +89,10 @@ public class MicroGActivity extends BaseSetupWizardActivity {
 
     @Override
     public void onNextPressed() {
+        if (enableLocation.isChecked() && ONLINE_SOURCE_UNSET.equals(selectedOnlineSource)) {
+            onlineSourceRequiredError.setVisibility(View.VISIBLE);
+            return;
+        }
         for (String packageId : MICROG_PACKAGES) {
             setAppEnabled(packageId, enableSwitch.isChecked());
         }
@@ -86,12 +102,16 @@ public class MicroGActivity extends BaseSetupWizardActivity {
                     "org.microg.gms.provision.ProvisionService");
             intent.putExtra("checkin_enabled", enablePush.isChecked());
             intent.putExtra("gcm_enabled", enablePush.isChecked());
-            intent.putExtra("wifi_mls", enableLocation.isChecked());
-            intent.putExtra("cell_mls", enableLocation.isChecked());
+            intent.putExtra("wifi_ichnaea", enableLocation.isChecked());
+            intent.putExtra("cell_ichnaea", enableLocation.isChecked());
             intent.putExtra("wifi_learning", enableLocation.isChecked());
             intent.putExtra("cell_learning", enableLocation.isChecked());
             intent.putExtra("nominatim_enabled", enableLocation.isChecked());
-            intent.putExtra("safetynet_enabled", false);
+            if (!ONLINE_SOURCE_UNSET.equals(selectedOnlineSource)) {
+                intent.putExtra("online_source_id", selectedOnlineSource);
+                android.util.Log.i("debuggy", "online_source_id: " + selectedOnlineSource);
+            }
+            intent.putExtra("cell_learning", enableLocation.isChecked());
             startService(intent);
         }
         super.onNextPressed();
@@ -100,5 +120,42 @@ public class MicroGActivity extends BaseSetupWizardActivity {
     private void setAppEnabled(String packageName, boolean enabled) {
         int state = enabled ? COMPONENT_ENABLED_STATE_ENABLED : COMPONENT_ENABLED_STATE_DISABLED;
         pm.setApplicationEnabledSetting(packageName, state, 0);
+    }
+
+    private void prepareOnlineSourcesList() {
+        for (int i = 0; i < onlineSourcesList.getChildCount(); i++) {
+            final View childView = onlineSourcesList.getChildAt(i);
+            if (!childView.isClickable() || !childView.isFocusable()) {
+                continue;
+            }
+            childView.setOnClickListener(view -> {
+                // The online source ID is specified as a tag in the microg_activity layout
+                // via strings in calyx_strings.xml.
+                selectedOnlineSource = (String) view.getTag(R.id.online_source_id);
+                onlineSourceRequiredError.setVisibility(View.INVISIBLE);
+                highlightView(view);
+            });
+        }
+        enableLocation.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            onlineSourcesList.setVisibility(isChecked ? View.VISIBLE : View.GONE);
+        });
+    }
+
+    private void highlightView(final View view) {
+        for (int i = 0; i < onlineSourcesList.getChildCount(); i++) {
+            final View childView = onlineSourcesList.getChildAt(i);
+            if (!childView.isClickable() || !childView.isFocusable()) {
+                continue;
+            }
+            if (childView.equals(view)) {
+                view.setBackground(
+                    ContextCompat.getDrawable(this, R.drawable.rectangle_background_focused)
+                );
+            } else {
+                childView.setBackground(
+                    ContextCompat.getDrawable(this, R.drawable.rectangle_background)
+                );
+            }
+        }
     }
 }
