70306a57f2412ef23dbb8c3dbad59d88576fe8dd
Michael Bestas <mkbestas@gmail.com>
2025-11-16 16:38:12 +0200

    Revert "Disable PrivExts"
    This reverts commit a3a2fc3a6652da540871ea74d72fe572ebe521f4.
    
    Reason for revert: Privileged extensions have been removed completely.
    
    Change-Id: I992e869ad1620a47c33deb1e817cc4fa2a91b8d6

--- src/org/lineageos/setupwizard/util/SetupWizardUtils.java
+++ src/org/lineageos/setupwizard/util/SetupWizardUtils.java
@@ -70,12 +70,8 @@ public class SetupWizardUtils {
 
     private static final String PROP_BUILD_DATE = "ro.build.date.utc";
 
-    private static final String AURORA_SERVICES_PACKAGE = "com.aurora.services";
     private static final String AURORA_STORE_PACKAGE = "com.aurora.store";
     private static final String FDROID_BASIC_PACKAGE = "org.fdroid.basic";
-    private static final String FDROID_PRIVEXT_PACKAGE = "org.fdroid.fdroid.privileged";
-    private static final List<String> LEGACY_PACKAGES =
-            List.of(AURORA_SERVICES_PACKAGE, FDROID_PRIVEXT_PACKAGE);
     private static final List<String> PACKAGE_INSTALLERS =
             List.of(FDROID_BASIC_PACKAGE, AURORA_STORE_PACKAGE);
 
@@ -193,7 +189,6 @@ public class SetupWizardUtils {
 
         handleNavigationOption();
         provisionDefaultUserAppPermissions(context);
-        disableLegacyApps(context);
         sendMicroGCheckInBroadcast(context);
         WallpaperManager.getInstance(context).forgetLoadedWallpaper();
         disableHome(context);
@@ -347,17 +342,6 @@ public class SetupWizardUtils {
                     AURORA_STORE_PACKAGE);
         } catch (RemoteException e) {
             Log.e(TAG, "Failed to add " + AURORA_STORE_PACKAGE + " to power save allowlist", e);
-        }
-    }
-
-    private static void disableLegacyApps(Context context) {
-        for (String packageName : LEGACY_PACKAGES) {
-            try {
-                context.getPackageManager().setApplicationEnabledSetting(packageName,
-                        COMPONENT_ENABLED_STATE_DISABLED, 0);
-            } catch (Exception e) {
-                Log.e(TAG, "Failed to disable " + packageName, e);
-            }
         }
     }
 
